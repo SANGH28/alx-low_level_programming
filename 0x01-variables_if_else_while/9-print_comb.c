1407b0615a6c95b8564b32c79fdeca83d7281f6b
SANGH28 <siloam098@gmail.com>
2023-02-19 10:43:43 -0800

    rite a program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,13 @@ int main(void)
 {
 int i;
 for (i = '0' ; i <= '9' ; i++)
+{	putchar(i);
+if (i != '9')
 {
-if (i == '9')
-	putchar(i);
-else if (i != '9')
-{
-putchar(i);
 putchar(',');
-}
-}
 putchar(' ');
+}
+}
+putchar('\n');
 return (0);
 }
