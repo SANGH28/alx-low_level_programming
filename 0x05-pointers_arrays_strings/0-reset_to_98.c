a706527d91de910a099646f9d8a334f0947f27fa
SANGH28 <siloam098@gmail.com>
2023-02-28 14:43:44 -0800

    Write a function that prints every other character of a string, starting with the first character, followed by a new line

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - function updates the value it points to to 98
+ *
+ * @n: parameter value
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n;
+
+	p = 402;
+}
