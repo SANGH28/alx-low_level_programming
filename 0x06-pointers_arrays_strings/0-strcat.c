d71e85181412ecda9991b767299f3e6ed7e7bdd2
SANGH28 <siloam098@gmail.com>
2023-03-03 07:06:32 -0800

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcat - This function appends the src string
+ * @dest: destination string
+ * @src: source spring
+ * Return: return destination spring
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= srclen ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
